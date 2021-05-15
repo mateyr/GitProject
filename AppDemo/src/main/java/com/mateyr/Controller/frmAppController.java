8bec5c4093a6649263b1874703ac9a1f5d97130a
MateyR <rodianmatey@gmail.com>
2021-05-14 20:07:17 -0700

    I have created the Contructor

---
+++ AppDemo/src/main/java/com/mateyr/Controller/frmAppController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mateyr.Controller;
+
+import com.mateyr.View.frmApp;
+
+/**
+ *
+ * @author MateyR
+ */
+public class frmAppController {
+    private frmApp frmAppController;
+
+    public frmAppController(frmApp frmAppController) {
+        this.frmAppController = frmAppController;
+        InitComponet();
+    }
+    
+    private void InitComponet(){
+        
+    }
+}
